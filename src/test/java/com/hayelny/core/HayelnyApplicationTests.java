3c3948c0bca65e3a494fdcd7df3c9b2ea65e5392
Jan Youssef <janyoussef19@gmail.com>
2023-05-10 09:03:44 +0300

    init project

---
+++ src/test/java/com/hayelny/core/HayelnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayelny.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HayelnyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
