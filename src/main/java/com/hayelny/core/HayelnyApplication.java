93be59dccef0356486f4755b10de74b5f6d452b3
Jan Youssef <janyoussef19@gmail.com>
2023-05-11 13:15:46 +0300

    check if image directory exists on startup

--- src/main/java/com/hayelny/core/HayelnyApplication.java
+++ src/main/java/com/hayelny/core/HayelnyApplication.java
@@ -1,31 +1,29 @@
 package com.hayelny.core;
 
-import com.hayelny.core.diagnosis.Diagnosis;
 import com.hayelny.core.diagnosis.DiagnosisRepo;
-import com.hayelny.core.diagnosis.DiagnosisStatus;
-import com.hayelny.core.diagnosis.Judgement;
-import com.hayelny.core.patient.Gender;
-import com.hayelny.core.patient.Patient;
+import com.hayelny.core.doctor.DoctorRepo;
 import com.hayelny.core.patient.PatientRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @SpringBootApplication
+@ServletComponentScan
 public class HayelnyApplication {
 
     final DiagnosisRepo diagnosisRepo;
     final PatientRepo patientRepo;
+    final DoctorRepo doctorRepo;
 
-    public HayelnyApplication(DiagnosisRepo diagnosisRepo, PatientRepo patientRepo) {
+    public HayelnyApplication(DiagnosisRepo diagnosisRepo, PatientRepo patientRepo, DoctorRepo doctorRepo) {
         this.diagnosisRepo = diagnosisRepo;
         this.patientRepo = patientRepo;
+        this.doctorRepo = doctorRepo;
     }
 
     public static void main(String[] args) {
@@ -35,26 +33,11 @@ public class HayelnyApplication {
     @Bean
     public CommandLineRunner commandLineRunner() {
         return args -> {
-            var p = new Patient();
-            p.setFirstName("new Ahmed");
-            p.setLastName("new Mohamed");
-            p.setDob(LocalDate.now());
-            p.setGender(Gender.MALE);
+            final Path IMAGE_DIRECTORY = Path.of("../images");
 
-            var d = new Diagnosis();
-            d.setJudgement(Judgement.POSITIVE);
-            d.setStatus(DiagnosisStatus.COMPLETED);
-            d.setConfidence(90D);
-            d.setPatient(p);
-            p.addDiagnosis(d);
-            patientRepo.save(p);
-            diagnosisRepo.save(d);
-
-            var list = patientRepo.findAll();
-            var saved = list.get(list.size()-1);
-            System.out.println(p);
-            System.out.println(saved);
-
+            if (Files.notExists(IMAGE_DIRECTORY)) {
+                Files.createDirectory(IMAGE_DIRECTORY);
+            }
         };
     }
 }
