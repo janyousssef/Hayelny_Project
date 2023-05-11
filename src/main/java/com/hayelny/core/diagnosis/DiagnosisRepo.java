45bb8fc48b3d1514ad20890d6517e3eb55201b6b
Jan Youssef <janyoussef19@gmail.com>
2023-05-11 14:13:36 +0300

    add method used to prevent duplicate Diagnoses for same image

--- src/main/java/com/hayelny/core/diagnosis/DiagnosisRepo.java
+++ src/main/java/com/hayelny/core/diagnosis/DiagnosisRepo.java
@@ -3,6 +3,10 @@ package com.hayelny.core.diagnosis;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DiagnosisRepo extends JpaRepository<Diagnosis, Long> {
+    Optional<Diagnosis> findByImage_Id(Long imageId);
+    Boolean existsByImage_Id(Long imageId);
 }
