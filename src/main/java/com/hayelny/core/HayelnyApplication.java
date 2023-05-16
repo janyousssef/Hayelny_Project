b9ee723028c6c2c76ca74d2cf1c5f6d197ce631b
Jan Youssef <janyoussef19@gmail.com>
2023-05-16 10:23:26 +0300

    set parallelism manually and refactor

--- src/main/java/com/hayelny/core/HayelnyApplication.java
+++ src/main/java/com/hayelny/core/HayelnyApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -27,17 +28,36 @@ public class HayelnyApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(HayelnyApplication.class, args);
+        createImageDirectoryIfMissing();
+        setParallelism();
+        SpringApplication.run(HayelnyApplication.class,
+                              args);
+    }
+
+    private static void setParallelism() {
+        int availableProcessors = Runtime
+                .getRuntime()
+                .availableProcessors();
+        String desiredParallelism = availableProcessors > 4 ? String.valueOf(availableProcessors) : "4";
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism",
+                           desiredParallelism);
+    }
+
+    private static void createImageDirectoryIfMissing() {
+        final Path IMAGE_DIRECTORY = Path.of("../images");
+
+        if (Files.notExists(IMAGE_DIRECTORY)) {
+            try {
+                Files.createDirectory(IMAGE_DIRECTORY);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Bean
     public CommandLineRunner commandLineRunner() {
         return args -> {
-            final Path IMAGE_DIRECTORY = Path.of("../images");
-
-            if (Files.notExists(IMAGE_DIRECTORY)) {
-                Files.createDirectory(IMAGE_DIRECTORY);
-            }
         };
     }
 }
