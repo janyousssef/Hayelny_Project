edf77d5895be52418c77a150aa1ca0d985a6ea6d
Jan Youssef <janyoussef19@gmail.com>
2023-05-16 11:13:03 +0300

    convert DTOs so they can be subclassed reflectively for HATEOAS

--- src/main/java/com/hayelny/core/diagnosis/DiagnosisDTO.java
+++ src/main/java/com/hayelny/core/diagnosis/DiagnosisDTO.java
@@ -1,14 +1,119 @@
 package com.hayelny.core.diagnosis;
 
-public record DiagnosisDTO(Long imageId, Long patientId, Long diagnosisId, String disease, String judgement, String confidence,
-                           String diagnosisStatus) {
-    public static DiagnosisDTO from(Diagnosis diagnosis) {
-        return new DiagnosisDTO(diagnosis.getImage().getId(),
-                                diagnosis.getPatient() == null ? null : diagnosis.getPatient().getId(),
-                                diagnosis.getId(),
-                                diagnosis.getDisease()== null ? null : diagnosis.getDisease().name(),
-                                diagnosis.getJudgement() == null ? null : diagnosis.getJudgement().name(),
-                                diagnosis.getConfidence() == null ? null : diagnosis.getConfidence().toString(),
-                                diagnosis.getStatus() == null ? null : diagnosis.getStatus().name());
+import java.util.Objects;
+
+public class DiagnosisDTO {
+    private final Long imageId;
+    private final Long patientId;
+    private final Long diagnosisId;
+    private final String disease;
+    private final String judgement;
+    private final String confidence;
+    private final String diagnosisStatus;
+
+    public DiagnosisDTO(Long imageId, Long patientId, Long diagnosisId, String disease, String judgement, String confidence,
+                        String diagnosisStatus) {
+        this.imageId = imageId;
+        this.patientId = patientId;
+        this.diagnosisId = diagnosisId;
+        this.disease = disease;
+        this.judgement = judgement;
+        this.confidence = confidence;
+        this.diagnosisStatus = diagnosisStatus;
     }
+
+    public static DiagnosisDTO from(Diagnosis diagnosis) {
+        return new DiagnosisDTO(diagnosis
+                                        .getImage()
+                                        .getId(),
+                                diagnosis.getPatient() == null ? null : diagnosis
+                                        .getPatient()
+                                        .getId(),
+                                diagnosis.getId(),
+                                diagnosis.getDisease() == null ? null : diagnosis
+                                        .getDisease()
+                                        .name(),
+                                diagnosis.getJudgement() == null ? null : diagnosis
+                                        .getJudgement()
+                                        .name(),
+                                diagnosis.getConfidence() == null ? null : diagnosis
+                                        .getConfidence()
+                                        .toString(),
+                                diagnosis.getStatus() == null ? null : diagnosis
+                                        .getStatus()
+                                        .name());
+    }
+
+    public Long getImageId() {
+        return imageId;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public Long getDiagnosisId() {
+        return diagnosisId;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public String getJudgement() {
+        return judgement;
+    }
+
+    public String getConfidence() {
+        return confidence;
+    }
+
+    public String getDiagnosisStatus() {
+        return diagnosisStatus;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (DiagnosisDTO) obj;
+        return Objects.equals(this.imageId,
+                              that.imageId) &&
+                Objects.equals(this.patientId,
+                               that.patientId) &&
+                Objects.equals(this.diagnosisId,
+                               that.diagnosisId) &&
+                Objects.equals(this.disease,
+                               that.disease) &&
+                Objects.equals(this.judgement,
+                               that.judgement) &&
+                Objects.equals(this.confidence,
+                               that.confidence) &&
+                Objects.equals(this.diagnosisStatus,
+                               that.diagnosisStatus);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(imageId,
+                            patientId,
+                            diagnosisId,
+                            disease,
+                            judgement,
+                            confidence,
+                            diagnosisStatus);
+    }
+
+    @Override
+    public String toString() {
+        return "DiagnosisDTO[" +
+                "imageId=" + imageId + ", " +
+                "patientId=" + patientId + ", " +
+                "diagnosisId=" + diagnosisId + ", " +
+                "disease=" + disease + ", " +
+                "judgement=" + judgement + ", " +
+                "confidence=" + confidence + ", " +
+                "diagnosisStatus=" + diagnosisStatus + ']';
+    }
+
 }
--- src/main/java/com/hayelny/core/images/ImageDTO.java
+++ src/main/java/com/hayelny/core/images/ImageDTO.java
@@ -1,4 +1,36 @@
 package com.hayelny.core.images;
 
-record ImageDTO(String message) {
+import java.util.Objects;
+
+public class ImageDTO {
+    private final String message;
+
+    public ImageDTO(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ImageDTO) obj;
+        return Objects.equals(this.message,
+                              that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
+    }
+
+    @Override
+    public String toString() {
+        return "ImageDTO[" +
+                "message=" + message + ']';
+    }
+
 }
