790902c977f4f4e2b43afc8a468822613e7835da
Jan Youssef <janyoussef19@gmail.com>
2023-05-18 01:32:36 +0300

    minor edit

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,2 @@
 #!bin/sh
-rm -f nohup.out && nohup sudo java -Dspring.profiles.active=prod -jar ../target/core-0.0.1-SNAPSHOT.jar
+rm -f nohup.out && nohup sudo java -Dspring.profiles.active=prod -jar ./target/core-0.0.1-SNAPSHOT.jar
