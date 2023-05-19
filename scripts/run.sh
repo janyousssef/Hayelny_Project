e4e6a46bba8218bdc3bf524506f1ae090a3c60e7
Jan Youssef <janyoussef19@gmail.com>
2023-05-19 13:54:05 +0300

    update scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,2 @@
 #!bin/sh
-rm -f backend.out && nohup sudo java -Dspring.profiles.active=prod -jar ./target/core-0.0.1-SNAPSHOT.jar backend.out 2>&1 &
+rm -f backend.out && nohup sudo java -Dspring.profiles.active=prod -jar ./target/core-0.0.1-SNAPSHOT.jar > backend.out 2>&1 &
