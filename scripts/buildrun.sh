56848e15553e61f4041760a81ce6fc3ffe5cf9c3
Jan Youssef <janyoussef19@gmail.com>
2023-05-19 13:39:32 +0300

    update scripts

--- scripts/buildrun.sh
+++ scripts/buildrun.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn clean package -Dspring.profiles.active=prod && rm -f nohup.out && nohup sudo java -Dspring.profiles.active=prod -jar ./target/core-0.0.1-SNAPSHOT.jar
+mvn clean package -Dspring.profiles.active=prod && rm -f backend.out && nohup sudo java -Dspring.profiles.active=prod -jar ./target/core-0.0.1-SNAPSHOT.jar backend.out 2>&1 &
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,2 @@
 #!bin/sh
-rm -f nohup.out && nohup sudo java -Dspring.profiles.active=prod -jar ./target/core-0.0.1-SNAPSHOT.jar
+rm -f backend.out && nohup sudo java -Dspring.profiles.active=prod -jar ./target/core-0.0.1-SNAPSHOT.jar backend.out 2>&1 &
