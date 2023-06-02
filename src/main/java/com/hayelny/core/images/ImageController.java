4bc169f31e6b7c66300abe6c0948962a61729bbf
Jan Youssef <janyoussef19@gmail.com>
2023-06-03 01:58:22 +0300

    add HATEOAS links to /{diagnosis}/diagnosis

--- src/main/java/com/hayelny/core/images/ImageController.java
+++ src/main/java/com/hayelny/core/images/ImageController.java
@@ -1,6 +1,7 @@
 package com.hayelny.core.images;
 
 import com.hayelny.core.diagnosis.Diagnosis;
+import com.hayelny.core.diagnosis.DiagnosisController;
 import com.hayelny.core.diagnosis.DiagnosisDTO;
 import com.hayelny.core.diagnosis.DiagnosisRepo;
 import jakarta.persistence.EntityNotFoundException;
@@ -10,11 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -22,40 +18,49 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 @RequestMapping(value = "/images")
 public class ImageController {
-    private final StorageService storageService;
+    private final ImageStorageService storageService;
     private final DiagnosisRepo diagnosisRepo;
 
-    public ImageController(StorageService storageService, DiagnosisRepo diagnosisRepo) {
+    public ImageController(ImageStorageService storageService, DiagnosisRepo diagnosisRepo) {
         this.storageService = storageService;
         this.diagnosisRepo = diagnosisRepo;
     }
 
+
     @CrossOrigin
     @PostMapping(value = "", consumes = "multipart/form-data")
-    public ResponseEntity<?> uploadImage(@RequestParam MultipartFile image) throws IOException {
+    public ResponseEntity<?> uploadImage(@RequestParam MultipartFile image) {
         String imageId = storageService.persist(image);
         ImageDTO dto = new ImageDTO("Image uploaded successfully");
+
         EntityModel<ImageDTO> entityModel = EntityModel.of(dto)
-                .add(linkTo(methodOn(ImageController.class).getImages(String.valueOf(imageId))).withRel("self"))
+                .add(linkTo(methodOn(ImageController.class).getImage(String.valueOf(imageId))).withRel("self"))
                 .add(linkTo(methodOn(ImageController.class).getDiagnosis(imageId)).withRel("diagnosis"));
+
         return ResponseEntity.status(201)
                 .body(entityModel);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<?> getImages(@PathVariable String id) throws IOException {
-        byte[] bytes = Files.readAllBytes(Path.of(".." + File.separator + "images" + File.separator + id + ".png"));
+    public ResponseEntity<?> getImage(@PathVariable String id) {
+        byte[] bytes = storageService.getImageAsBytes(id);
         return ResponseEntity.ok()
                 .contentType(MediaType.IMAGE_PNG)
                 .body(bytes);
     }
 
     @GetMapping(value = "/{id}/diagnosis")
-    public DiagnosisDTO getDiagnosis(@PathVariable String id) {
+    public EntityModel<DiagnosisDTO> getDiagnosis(@PathVariable String id) {
         Diagnosis diagnosis = diagnosisRepo.findByImage_Id(id)
                 .orElseThrow(() -> new EntityNotFoundException("No diagnosis for image with id = " + id + " found."));
 
-        return DiagnosisDTO.from(diagnosis);
+        DiagnosisDTO dto = DiagnosisDTO.from(diagnosis);
+        EntityModel<DiagnosisDTO> entityModel = EntityModel.of(dto);
+
+        entityModel.add(linkTo(methodOn(ImageController.class).getImage(dto.getImageId())).withRel("image"));
+        entityModel.add(linkTo(methodOn(DiagnosisController.class).getDiagnosis(dto.getDiagnosisId())).withRel("self"));
+
+        return entityModel;
     }
 
 }
