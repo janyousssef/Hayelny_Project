e1551ab79848b8a6d57724089338a0ecae614a53
Jan Youssef <janyoussef19@gmail.com>
2023-06-05 03:34:05 +0300

    Replace use of ImageDTO with Message

---
+++ src/main/java/com/hayelny/core/Message.java
@@ -0,0 +1,4 @@
+package com.hayelny.core;
+
+public record Message(String message) {
+}
--- src/main/java/com/hayelny/core/RootController.java
+++ src/main/java/com/hayelny/core/RootController.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-record Message(String message) {
-}
-
 @Controller
 @RequestMapping(value = "/")
 public class RootController {
