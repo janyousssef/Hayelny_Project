a51aec44c52459d09ac0803ab21e1ee79971e086
Jan Youssef <janyoussef19@gmail.com>
2023-06-27 12:24:32 +0300

    delegate getting the response to the Adapter class

---
+++ src/main/java/com/hayelny/core/diagnosis/DiagnosisResponseAdapter.java
@@ -0,0 +1,31 @@
+package com.hayelny.core.diagnosis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class DiagnosisResponseAdapter {
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    Diagnosis getNewDiagnosisFor(String id) {
+        Map<?, ?> responseJson = restTemplate.getForObject("http://localhost:8000/model?id=" + id,
+                                                           Map.class);
+        DiagnosisResponse diagnosisResponse = new DiagnosisResponse(responseJson.get("prediction").toString(),
+                                                                    responseJson.get("diagnosis").toString());
+        return createDiagnosisFrom(diagnosisResponse);
+    }
+
+
+    Diagnosis createDiagnosisFrom(DiagnosisResponse response) {
+        Diagnosis diagnosis = new Diagnosis();
+        diagnosis.setConfidence(response.getConfidence());
+        diagnosis.setJudgement(response.getJudgement());
+        diagnosis.setDisease(Disease.PNEUMONIA);
+        diagnosis.setStatus(DiagnosisStatus.COMPLETED);
+        return diagnosis;
+    }
+}
--- src/main/java/com/hayelny/core/diagnosis/DiagnosisService.java
+++ src/main/java/com/hayelny/core/diagnosis/DiagnosisService.java
@@ -2,58 +2,45 @@ package com.hayelny.core.diagnosis;
 
 
 import com.hayelny.core.images.ImageRepo;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ForkJoinPool;
 
 @Service
+@RequiredArgsConstructor
 public class DiagnosisService {
 
     private final DiagnosisRepo diagnosisRepo;
     private final ImageRepo imageRepo;
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final DiagnosisResponseAdapter diagnosisResponseAdapter;
 
-    public DiagnosisService(DiagnosisRepo diagnosisRepo, ImageRepo imageRepo) {
-        this.diagnosisRepo = diagnosisRepo;
-        this.imageRepo = imageRepo;
-    }
 
-    public void diagnose(final String id) {
-        Runnable runnable = () -> getDiagnosis(id);
-        try(ForkJoinPool pool = ForkJoinPool.commonPool() ) {
-            pool.execute(runnable);
+    public void diagnose(final String imageId) {
+        try (ForkJoinPool pool = ForkJoinPool.commonPool()) {
+            pool.execute(() -> getDiagnosis(imageId));
         }
 
     }
 
-    public Optional<Diagnosis> findByImageId(String id) {
-        return diagnosisRepo.findByImage_Id(id);
+    public Diagnosis findByImageId(String id) {
+        return diagnosisRepo.findByImage_Id(id)
+                .orElseThrow(() -> new EntityNotFoundException("No diagnosis for image with id = " + id + " found."));
     }
 
-    private void getDiagnosis(String id) {
-        Map<?,?> responseJson = restTemplate.getForObject("http://localhost:8000/model?id=" + id,
-                                                  Map.class);
-        DiagnosisResponse response = new DiagnosisResponse(responseJson.get("prediction").toString(),
-                                                           responseJson.get("diagnosis").toString());
-        diagnosisRepo.findByImage_Id(id)
-                .map(diagnosis -> populateDiagnosis(diagnosis, response))
-                .orElseGet(() -> {
-                    Diagnosis diagnosis = new Diagnosis();
-                    diagnosis.setImage(imageRepo.getReferenceById(id));
-                    populateDiagnosis(diagnosis, response);
-                    return diagnosisRepo.save(diagnosis);
-                });
-    }
+    private Diagnosis getDiagnosis(String imageId) {
+        Optional<Diagnosis> diagnosisOptional = diagnosisRepo.findByImage_Id(imageId);
 
-    private Diagnosis populateDiagnosis(Diagnosis diagnosis, DiagnosisResponse response) {
-        diagnosis.setConfidence(response.getConfidence());
-        diagnosis.setJudgement(response.getJudgement());
-        diagnosis.setDisease(Disease.PNEUMONIA);
-        diagnosis.setStatus(DiagnosisStatus.COMPLETED);
+        if (diagnosisOptional.isPresent()) {
+            return diagnosisOptional.get();
+        }
+
+        Diagnosis diagnosis = diagnosisResponseAdapter.getNewDiagnosisFor(imageId);
+        diagnosis.setImage(imageRepo.getReferenceById(imageId));
         return diagnosisRepo.save(diagnosis);
     }
+
 
 }
