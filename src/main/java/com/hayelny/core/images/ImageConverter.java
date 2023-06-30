bc98c8529b8225443fc6969c7d62caca9a7554a8
Jan Youssef <janyoussef19@gmail.com>
2023-06-30 19:12:30 +0300

    [fix] fix growing self referencing command

--- src/main/java/com/hayelny/core/images/ImageConverter.java
+++ src/main/java/com/hayelny/core/images/ImageConverter.java
@@ -6,19 +6,23 @@ import java.io.IOException;
 
 @Service
 public class ImageConverter {
-    private final String[] CMD_ARR = {"/bin/bash", "-c", ""};
+    private final String[] CMD_ARR = {"/bin/bash", "-c", "dcmj2pnm +oj +Jq 95 ","",""};
 
     public void convertToJpeg(String path) {
         //requires dcmj2pnm to be installed
         //runs on linux
-        String cmd = "dcmj2pnm +oj +Jq 95 ";
-        CMD_ARR[2] = cmd + path + " " + path + ".jpg";
+        CMD_ARR[3] = path;
+        CMD_ARR[4] = path + ".jpg";
         System.out.println(CMD_ARR[2]);
         System.out.println("converting image");
         try {
-            Runtime.getRuntime().exec(CMD_ARR);
+            Process exec = Runtime.getRuntime().exec(CMD_ARR);
+            exec.waitFor();
+            System.out.println(exec.exitValue());
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
         System.out.println("converting image done");
     }
